bb3a37e442836111a80a4c4e8d640d018a635e6a
nsoftnhdev <153261745+nsoftnhdev@users.noreply.github.com>
2025-08-11 22:25:45 +0800

    backend complete

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -2,6 +2,8 @@ import { Inngest } from "inngest";
 import User from "../models/User.js";
 import Connection from "../models/Connections.js";
 import sendEmail from "../configs/nodeMailer.js";
+import Story from "../models/Story.js";
+import Message from "../models/Message.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "picoverse-app" });
@@ -79,7 +81,7 @@ const sendNewConnectionRequestReminder = inngest.createFunction(
       <p>You have a new connection request from ${connection.from_user_id.full_name} - @${connection.from_user_id.username}</p>
       <p>Click <a href="${process.env.FRONTEND_URL}/connections" style="color: #10b981;">here</a> to accept or reject the request</p>
       <br/>
-      <p>Thanks, <br/>Picoverse Stay Connected</p>
+      <p>Thanks, <br/>Picoverse - Stay Connected</p>
       </div>`;
 
       await sendEmail({
@@ -101,7 +103,7 @@ const sendNewConnectionRequestReminder = inngest.createFunction(
       }
 
       const subject = `👋 New Connection Request`;
-      const body = `<div style="font-family: Arial, sans-serif; padding 20px;"
+      const body = `<div style="font-family: Arial, sans-serif; padding 20px;">
       <h2>Hi ${connection.to_user_id.full_name},</h2>
       <p>You have a new connection request from ${connection.from_user_id.full_name} - @${connection.from_user_id.username}</p>
       <p>Click <a href="${process.env.FRONTEND_URL}/connections" style="color: #10b981;">here</a> to accept or reject the request</p>
@@ -120,10 +122,62 @@ const sendNewConnectionRequestReminder = inngest.createFunction(
   }
 );
 
+// Inngest function to delete story after 24 hours
+const deleteStory = inngest.createFunction(
+  { id: "story-delete" },
+  { event: "app/story.delete" },
+  async ({ event, step }) => {
+    const { storyId } = event.data;
+    const in24Hours = new Date(Date.now() + 24 * 60 * 60 * 1000);
+    await step.sleepUntil("wait-for-24-hours", in24Hours);
+    await step.run("delete-story", async () => {
+      await Story.findByIdAndDelete(storyId);
+      return { message: "Story deleted." };
+    });
+  }
+);
+
+const sendNotificationOfUnseenMessages = inngest.createFunction(
+  { id: "send-unseen-messages-notification" },
+  { cron: "TZ=Malaysia/Kuala_Lumpur 0 9 * * *" }, // Everyday 9 AM\
+  async ({ step }) => {
+    const messages = await Message.find({ seen: false }).populate("to_user_id");
+    const unseenCount = {};
+
+    messages.map((message) => {
+      unseenCount[message.to_user_id._id] =
+        (unseenCount[to_user_id._id] || 0) + 1;
+    });
+
+    for (const userId in unseenCount) {
+      const user = await User.findById(userId);
+
+      const subject = `📬 You have ${unseenCount[userId]} unseen messages`;
+
+      const body = `<div style="font-family: Arial, sans-serif; padding 20px;">
+        <h2>Hi ${user.full_name},</h2>
+        <p>You have ${unseenCount[userId]} unseen messages</p>
+        <p>Click <a href="${process.env.FRONTEND_URL}/messages" style="color: #10b981;">here</a> to view them</p>
+        <br/>
+      <p>Thanks, <br/>Picoverse - Stay Connected</p>
+      </div>`;
+
+      await sendEmail({
+        to: user.email,
+        subject,
+        body,
+      });
+    }
+    return { message: "Notification sent." };
+  }
+);
+
 // Create an empty array where we'll export future Inngest functions
 export const functions = [
   syncUserCreation,
   syncUserUpdation,
   syncUserDeletion,
   sendNewConnectionRequestReminder,
+  deleteStory,
+  sendNotificationOfUnseenMessages,
 ];
--- server/server.js
+++ server/server.js
@@ -6,6 +6,9 @@ import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
 import { clerkMiddleware } from "@clerk/express";
 import userRouter from "./routes/userRoutes.js";
+import postRouter from "./routes/postRoutes.js";
+import storyRouter from "./routes/storyRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
 
 const app = express();
 
@@ -17,7 +20,10 @@ app.use(clerkMiddleware());
 
 app.get("/", (req, res) => res.send("Server is running"));
 app.use("/api/inngest", serve({ client: inngest, functions }));
-app.use('/api/user', userRouter)
+app.use("/api/user", userRouter);
+app.use("/api/post", postRouter);
+app.use("/api/story", storyRouter);
+app.use("/api/message", messageRouter);
 
 const PORT = process.env.PORT || 4001;
 
