fc3cc2df8c51eb5ec0e2957b8444a17ec51782f4
nsoftnhdev <153261745+nsoftnhdev@users.noreply.github.com>
2025-08-12 22:30:50 +0800

    AI article writing web-based app added

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -51,12 +51,14 @@ const Feed = () => {
       <div className="max-xl:hidden sticky top-0">
         <div className="max-w-xs bg-white text-xs p-4 rounded-md inline-flex flex-col gap-2 shadow">
           <h3 className="text-slate-800 font-semibold">Sponsored</h3>
-          <img
-            src={assets.sponsored_img}
-            className="w-75 h-50 rounded-md"
-            alt=""
-          />
-          <p className="text-slate-600">Email marketing</p>
+          <a href="https://rapid-ai-beta.vercel.app/" target="_blank">
+            <img
+              src={assets.sponsored_img}
+              className="w-75 h-50 rounded-md"
+              alt=""
+            />
+          </a>
+          <p className="text-slate-600">AI Article Writing</p>
           <p className="text-slate-400">
             Supercharge your marketing with a powerful, easy-to-use platform
             built for results.
